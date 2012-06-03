c6a9112e23ef470ec736c7cd88057659388e26ec
Yunxing Dai <nov503@gmail.com>
2012-06-03 00:58:18 -0700

    updated help infomation, added function mapping

--- renren.py
+++ renren.py
@@ -45,7 +45,9 @@ class SendMsgBot(sleekxmpp.ClientXMPP):
     def _send_help_msg(self, jid):
         self.sendMessage(jid,u'\u5bf9\u6211\u4f7f\u7528'+
                          u'/twitter <keyword>\u8fdb\u884c'+
-                         u'twitter\u641c\u7d22')
+                         u'twitter\u641c\u7d22' +
+                         u',\u6bd4\u5982/twitter \u9648' +
+                         u'\u5149\u8bda, \u6216/twitter #freechen')
 
     def _fetch_twitter(self, query):
         query = "http://search.twitter.com/search.json?q="+\
@@ -54,39 +56,50 @@ class SendMsgBot(sleekxmpp.ClientXMPP):
         json_obj = json.loads(json_str)["results"]
         return [obj["text"] for obj in json_obj]
 
+
+    def _handle_twitter_query(self, jid, query):
+        print "User query:%s" % query
+        try:
+            replys = self._fetch_twitter(query)
+        except:
+            self.sendMessage(jid,
+                             "Someting wrong," +
+                             "please provide feedback to me,"+
+                             "many thanks!")
+            return
+
+        if len(replys)==0:
+            self.sendMessage(jid,
+                             "Sorry, no search results")
+            return
+
+        for r in replys:
+            self.sendMessage(jid, r)
+            #sleep because xiaonei's stupid implementation
+            #continuous packets get lost
+            time.sleep(1.2)
+
+        self.sendMessage(jid, u'\u641c\u7d22\u7ed3\u675f')
+
+    def _handle_help(self, jid, _msg):
+        self._send_help_msg(jid)
+
     def onMessage(self, message):
         msg = message['body'].strip()
         print "Get Msg:%s" % msg
         user = message['from'].user
 
-        if msg.startswith("/twitter"):
-            if len(msg.split(' ')) == 1:
-                _send_help_msg(self, message['from'])
-                return
+        m_f_dict = {"/twitter":self._handle_twitter_query,
+                    "/help":self._handle_help}
 
-            query = ' '.join(msg.split(' ')[1:])
-            try:
-                replys = self._fetch_twitter(query)
-            except:
-                self.sendMessage(message['from'],
-                                 "Someting wrong," +
-                                 "please provide feedback to me,"+
-                                 "many thanks!")
-                return
+        def forward(prefix, func, msg, jid):
+            if msg.startswith(prefix):
+                msg = ' '.join(msg.split(' ')[1:])
+                func(jid, msg)
 
-            if len(replys)==0:
-                self.sendMessage(message['from'],
-                                 "Sorry, no search results")
+        map(lambda x: forward(x, m_f_dict[x], msg, message['from']),
+            m_f_dict)
 
-            for r in replys:
-                self.sendMessage(message['from'], r)
-                #sleep because xiaonei's stupid implementation
-                #continuous packets get lost
-                time.sleep(1.2)
-
-
-        if "/help" in msg:
-            self._send_help_msg(message['from'])
 
     def failed(self, event):
         print "Failed Login"
